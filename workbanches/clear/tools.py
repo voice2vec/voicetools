986759ea504bf823b5a00c33beb6c825567b17b7
Andrey Tvorozhkov <atvorozhkov@yandex.ru>
2017-01-08 02:48:39 -0500

    Add example, some features in books.

--- workbanches/clear/tools.py
+++ workbanches/clear/tools.py
@@ -37,26 +37,6 @@ magnitude = magnitude_func()
 def sign_func(number):
     return number >= 0
 
-def zcr_func():
-    sample = T.vector(dtype='int64')
-    window_size = T.scalar(dtype = 'int16')
-    
-    a = sample[:sample.shape[0] - 1]
-    b = sample[1:]
-    
-    a, updates = theano.scan(fn=sign_func, sequences=a)
-    b, updates = theano.scan(fn=sign_func, sequences=b)
-    
-    sample_sq = np.abs(a - b)
-    padded_sample = T.concatenate([T.zeros([window_size]), sample_sq])
-    cumsum = T.cumsum(padded_sample)
-    zcr = cumsum[window_size:] - cumsum[:-window_size]
-    zcr = zcr / (2 * window_size)
-    return theano.function([sample, window_size], zcr)
-
-zcr = zcr_func()
-
-
 def predict_by_energy(signal, rate, window_size = 30, threshold = 0.3):
     '''
         :param signal: the array with signal
@@ -84,19 +64,4 @@ def predict_by_magnitude(signal, rate, window_size = 30, threshold = 0.3):
     magnitude_answer = [True if i > threshold else False for i in normalize(magnitude(signal, window_size))]
     
     return list(zip(signal, magnitude_answer))
-
-
-def predict_by_zcr(signal, rate, window_size = 30, threshold = 0.3):
-    '''
-        :param signal: the array with signal
-        :param rate: signal rate
-        :param window_size_30: size of frame in ms
-        :param threshold: threshold of prediction (from 0 to 1)
-    '''
-    
-    window_size = int(window_size * (rate/1000))
-    
-    zcr = [True if i > threshold else False for i in normalize(zcr(signal, window_size))]
-
-    return list(zip(signal, zcr))
 
